de21eb44ec2df9a88bec23fc56ac599685cf9f3c
vir dhoriya <virdhoriya@gmail.com>
2024-10-26 15:34:06 +0530

    Refactor: Move service-related component to the appropriate service section

--- src/components/more/More.jsx
+++ src/components/more/More.jsx
@@ -1,19 +1,7 @@
-import { useState } from "react";
-import BackgroundImage from "./BackgroundImage";
-import ChooseService from "./ChooseService";
-import ServiceContainer from "./ServiceContainer";
-
 const More = () => {
-  const defaultService = 1;
-  const [serviceSectionCategory, setServiceSectionCategory] = useState([]);
-  const [sid, setSid] = useState(defaultService);
-  const [paramId, setParamId] = useState(175);
-
   return (
     <div>
-      <BackgroundImage />
-      <ChooseService setServiceSectionCategory={setServiceSectionCategory} setSid={setSid} sid={sid} setParamId={setParamId}/>
-      <ServiceContainer serviceSectionCategory={serviceSectionCategory} sid={sid} setParamId={setParamId} paramId={paramId}/>
+      <p className="text-center">More Component</p>
     </div>
   );
 };
